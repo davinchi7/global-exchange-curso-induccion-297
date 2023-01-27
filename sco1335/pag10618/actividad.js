6175774f056db1804f44f5c44fef993376713d87
AngieArtv <angie.artv@gmail.com>
2023-01-27 12:09:37 +0100

    feat: actualizar tamaños mini

--- sco1335/pag10618/actividad.js
+++ sco1335/pag10618/actividad.js
@@ -1,22 +1,27 @@
-setTimeout(() => {
-    $('.js-caja-img').show().addClass('animate__slideInDown');
-    $(".js-caja-img").css("display","flex");
-}, 2000);
-setTimeout(() => {
-    $('.js-img-1').addClass('animate__animated animate__flash animate__slow animate__infinite');
-}, 3000);
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-for (let i = 0; i <= 3; i++){
-    $('.js-img-' + i +'').on('click', () => {
-        $('.js-img-' + i +'').removeClass('animate__animated animate__flash animate__slow animate__infinite');
-        $('.js-img-' + (i + 1) +'').addClass('animate__animated animate__flash animate__slow animate__infinite');
-        $('.js-cifra-' + i +'').addClass('animate__animated animate__slideInRight');
-        $('.js-cifra-' + i +'').css("display","flex");
-
-    });
-}
-$('.js-img-3').on('click', () => {
+    setTimeout(() => {
+        $('.js-caja-img').show().addClass('animate__slideInDown');
+        $(".js-caja-img").css("display","flex");
+    }, 2000);
+    setTimeout(() => {
+        $('.js-img-1').addClass('animate__animated animate__flash animate__slow animate__infinite');
+    }, 3000);
     
+    for (let i = 0; i <= 3; i++){
+        $('.js-img-' + i +'').on('click', () => {
+            $('.js-img-' + i +'').removeClass('animate__animated animate__flash animate__slow animate__infinite');
+            $('.js-img-' + (i + 1) +'').addClass('animate__animated animate__flash animate__slow animate__infinite');
+            $('.js-cifra-' + i +'').addClass('animate__animated animate__slideInRight');
+            $('.js-cifra-' + i +'').css("display","flex");
+    
+        });
+    }
+    $('.js-img-3').on('click', () => {
+        
+    });
+    
+    window.parent.document.getElementById('btnSig').style.display = 'block';
 });
 
-window.parent.document.getElementById('btnSig').style.display = 'block';
--- sco1335/pag10619/actividad.js
+++ sco1335/pag10619/actividad.js
@@ -1,16 +1,21 @@
-setTimeout(() => {
-    $('.js-mision').addClass('animate__animated animate__flash animate__slow animate__infinite');
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    setTimeout(() => {
+        $('.js-mision').addClass('animate__animated animate__flash animate__slow animate__infinite');
+        
+    }, 2000);
     
-}, 2000);
-
-$('.js-mision').on('click', () => {
-    $('.js-text-1').show().addClass('animate__slideInDown');
-    $('.js-mision').removeClass('animate__animated animate__flash animate__slow animate__infinite');
-    $('.js-vision').addClass('animate__animated animate__flash animate__slow animate__infinite');
+    $('.js-mision').on('click', () => {
+        $('.js-text-1').show().addClass('animate__slideInDown');
+        $('.js-mision').removeClass('animate__animated animate__flash animate__slow animate__infinite');
+        $('.js-vision').addClass('animate__animated animate__flash animate__slow animate__infinite');
+    });
+    $('.js-vision').on('click', () => {
+        $('.js-text-2').show().addClass('animate__slideInDown');
+        $('.js-vision').removeClass('animate__animated animate__flash animate__slow animate__infinite');
+    });
+    
+    window.parent.document.getElementById('btnSig').style.display = 'block';
 });
-$('.js-vision').on('click', () => {
-    $('.js-text-2').show().addClass('animate__slideInDown');
-    $('.js-vision').removeClass('animate__animated animate__flash animate__slow animate__infinite');
-});
 
-window.parent.document.getElementById('btnSig').style.display = 'block';
